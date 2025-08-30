c34be17cdf7e5cc9b35198d5be33b0e037227dc5
BSGProgram <brunogalindo2306@gmail.com>
2025-08-30 14:25:55 +0000

    Ajusta lógica de comparação para exibir empates e vencedores no Super Trunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -239,9 +239,11 @@ int main() {
     //COMPARACAO DE ATRIBUTOS
 
     int opcao, comparar, pontoturistico, populacao, pibpercapita, area, densidade;
+    int carta0 = 0;
     int carta1 = 1;
     int carta2 = 2;
-    char empate[20] = 'Empataram';
+    char empate[20] = "Empataram";
+    char vencedor[20];
     
     //int empate = 0;
     printf ("\nMENU - COMPARACAO DE ATRIBUTOS\n\n");
@@ -257,67 +259,67 @@ int main() {
 
     switch (opcao){
     case 1: //População
-        if (populacao1 > populacao2) {
+        if (populacao1 == populacao2) {
+            printf("As cidades %s e %s, %s com a mesma população\n", cidade1, cidade2, empate);
+            int carta0 = 0;
+        } else if (populacao1 > populacao2) {
             printf("A cidade %s tem maior população que a cidade %s\n", cidade1, cidade2);
             int carta1 = 1;
-        } else if (populacao2 > populacao1) {
+        } else {
             printf("A cidade %s tem maior população que a cidade %s\n", cidade2, cidade1);
             int carta2 = 2;
-        } else {
-            printf("As cidades %s e %s, %s com a mesma população\n", cidade1, cidade2, empate);
-            char empate[20] = 'Empataram';
         }
         break;
     
     case 2: //Pontos Turísticos
-        if (pontot1 > pontot2) {
-            printf("A cidade %s tem mais pontos turísticos que a cidade %s\n", cidade1, cidade2);
-            int carta1 = 1;  
-        } else if (pontot2 > pontot1) {
-            printf("A cidade %s tem mais pontos turísticos que a cidade %s\n", cidade2, cidade1);
-            int carta2 = 2;
-        } else {
+        if (pontot1 == pontot2) {
             printf("As cidades %s e %s, %s com a mesma quantidade de Pontos Turísticos\n", cidade1, cidade2, empate);
-            char empate[20] = 'Empataram';
+            int carta0 = 0;
+        } else if (populacao1 > populacao2) {
+            printf("A cidade %s tem maior quantidade de Pontos Turísticos %s\n", cidade1, cidade2);
+            int carta1 = 1;
+        } else {
+            printf("A cidade %s tem maior quantidade de Pontos Turísticos %s\n", cidade2, cidade1);
+            int carta2 = 2;
         }
         break;
 
-    case 3: //PIB Percapita
-        if (pib1 > pib2) {
+    case 3: //PIB
+        if (pib1 == pib2) {
+            printf("As cidades %s e %s, %s com mesmo PIB\n", cidade1, cidade2, empate);
+            int carta0 = 0;   
+        } else if (pib1 > pib2) {
             printf("A cidade %s tem maior PIB que a cidade %s\n", cidade1, cidade2);
-            int carta1 = 1;
-        } else if (pib2 > pib1) {
-            printf("A cidade %s tem maiOR PIB que a cidade %s\n", cidade2, cidade1);
-            int carta2 = 2;
+            int carta1 = 1;   
         } else {
-            printf("As cidades %s e %s, %s, com mesmo PIB\n", cidade1, cidade2, empate);
-            char empate[20] = 'Empataram';
+            printf("A cidade %s tem maior PIB que a cidade %s\n", cidade2, cidade1);
+            int carta2 = 2;
         }
         break;
 
     case 4: //Área
-        if (area1 > area2) {
-            printf("A cidade %s tem maior área que a cidade %s\n", cidade1, cidade2);
-            int carta1 = 1;
-        } else if (area2 > area1) {
-            printf("A cidade %s tem maior Área que a cidade %s\n", cidade2, cidade1);
-            int carta2 = 2;
+        if (area1 == area2) {
+            printf("As cidades %s e %s, %s com a mesma ÁREA\n", cidade1, cidade2, empate);
+            int carta0 = 0;   
+        } else if (area1 > area2) {
+            printf("A cidade %s tem maior ÁREA que a cidade %s\n", cidade1, cidade2);
+            int carta1 = 1;   
         } else {
-            printf("As cidades %s e %s, %s, com a mesma Área\n", cidade1, cidade2, empate);
-            char empate[20] = 'Empataram';
+            printf("A cidade %s tem maior ÁREA que a cidade %s\n", cidade2, cidade1);
+            int carta2 = 2;
         }
         break;
 
         case 5: //Densidade
-        if (densidade1 < densidade2) {
-            printf("A cidade %s tem menor Densidade Populacional que a cidade %s\n", cidade1, cidade2);
-            int carta1 = 1;
+        if (densidade1 == densidade2) {
+            printf("As cidades %s e %s, %s, com a mesma Densidade Populacional\n", cidade1, cidade2, empate);
+            int carta0 = 0;
         } else if (densidade2 < densidade1) {
+            printf("A cidade %s tem menor Densidade Populacional que a cidade %s\n", cidade1, cidade2);
+            int carta1 = 1;  
+        } else {
             printf("A cidade %s tem menor Densidade Populacional que a cidade %s\n", cidade2, cidade1);
             int carta2 = 2;
-        } else {
-            printf("As cidades %s e %s, %s, com a mesma Densidade Populacional\n", cidade1, cidade2, empate);
-            char empate[20] = 'Empataram';
         }
         break;
     default:
@@ -338,10 +340,22 @@ int main() {
 
     printf("Cidades Escolhidas foram: %s e %s\n", cidade1, cidade2);
     printf("O atributo usado na comparação foi: %d\n", opcao);
-    printf("Os valores do atributo para cada carta foram: %d e %d\n", populacao1, populacao2, densidade1, densidade2, area1, area2, pib1, pib2, pontot1, pontot2);
-    printf("A carta vencedora foi número %d\n", carta1, carta2);
-    printf("Ambas as cidades %s\n", empate);
+    printf("Os valores do atributo para cada carta foram: %d e %d\n", (populacao1, populacao2), (densidade1, densidade2), (area1, area2), (pib1, pib2), (pontot1, pontot2));
 
+    
+    if (vencedor == 0){
+    printf("Ambos atributos empataram\n");
+    } else {
+        printf("A carta vencedora foi a carta %d\n", vencedor == 1 ? 1 : 0);
+    } 
+    
+    if (vencedor == 1) {
+        printf("A Carta vencedora foi a carta 1\n");
+    } else {
+        printf("A Carta vencedora foi a carta 2\n");
+    }
+    
+    //printf("A vencedora foi A CARTA %d\n", carta1, carta2);
 
     return 0;
    
